567e7879469cd9ea52ad6b02543970d5e266e623
Nsuku <nsuku.chauke@dadevs.co.za>
2022-11-28 12:53:54 +0200

    updates

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -4,33 +4,33 @@ import './contact.css';
 export default function contact() {
     return (
     <div>
-         <div class="contact wow fadeInUp" data-wow-delay="0.1s" id="contact">
-            <div class="container-fluid">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-md-4"></div>
-                        <div class="col-md-8">
-                            <div class="contact-form">
+         <div className="contact wow fadeInUp" data-wow-delay="0.1s" id="contact">
+            <div className="container-fluid">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-md-4"></div>
+                        <div className="col-md-8">
+                            <div className="contact-form">
                                 <div id="success"></div>
-                                <form name="sentMessage" id="contactForm" novalidate="novalidate">
-                                    <div class="control-group">
+                                <form className="sentMessage" id="contactForm" novalidate="novalidate">
+                                    <div className="control-group">
                                         <input type="text" class="form-control" id="name" placeholder="Your Name" required="required" data-validation-required-message="Please enter your name" />
-                                        <p class="help-block"></p>
+                                        <p className="help-block"></p>
                                     </div>
-                                    <div class="control-group">
+                                    <div className="control-group">
                                         <input type="email" class="form-control" id="email" placeholder="Your Email" required="required" data-validation-required-message="Please enter your email" />
-                                        <p class="help-block"></p>
+                                        <p className="help-block"></p>
                                     </div>
-                                    <div class="control-group">
+                                    <div className="control-group">
                                         <input type="text" class="form-control" id="subject" placeholder="Subject" required="required" data-validation-required-message="Please enter a subject" />
-                                        <p class="help-block"></p>
+                                        <p className="help-block"></p>
                                     </div>
-                                    <div class="control-group">
-                                        <textarea class="form-control" id="message" placeholder="Message" required="required" data-validation-required-message="Please enter your message"></textarea>
-                                        <p class="help-block"></p>
+                                    <div className="control-group">
+                                        <textarea className="form-control" id="message" placeholder="Message" required="required" data-validation-required-message="Please enter your message"></textarea>
+                                        <p className="help-block"></p>
                                     </div>
                                     <div>
-                                        <button class="btn" type="submit" id="sendMessageButton">Send Message</button>
+                                        <button className="btn" type="submit" id="sendMessageButton">Send Message</button>
                                     </div>
                                 </form>
                             </div>
